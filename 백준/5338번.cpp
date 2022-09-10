d8412202a242d047dad7b85a2dc7f6f8df6e5e84
2sseung1 <bigseung12@gmail.com>
2022-09-10 23:45:04 +0900

    Add : 마이크로소프트 로고

---
+++ 백준/5338번.cpp
@@ -0,0 +1,12 @@
+// 마이크로소프트 로고 : https://www.acmicpc.net/problem/5338
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "       _.-;;-._\n";
+	std::cout << "'-..-'|   ||   |\n";
+	std::cout << "'-..-'|_.-;;-._|\n";
+	std::cout << "'-..-'|   ||   |\n";
+	std::cout << "'-..-'|_.-''-._|\n";
+}
