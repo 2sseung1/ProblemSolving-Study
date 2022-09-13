55a8afc301fe533322859eb81b64da097bb2eb02
2sseung1 <bigseung12@gmail.com>
2022-09-13 23:37:03 +0900

    Add : 웰컴

---
+++ 백준/5337번.cpp
@@ -0,0 +1,10 @@
+// À£ÄÄ : https://www.acmicpc.net/problem/5337
+
+#include <iostream>
+
+int main()
+{
+	std::cout << ".  .   .\n";
+	std::cout << "|  | _ | _. _ ._ _  _\n";
+	std::cout << "|/\\|(/.|(_.(_)[ | )(/.\n";
+}
